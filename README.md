94527d138467089ed7e591ff72f2c45a72b42a0f
Poujhit <poujhit112313@gmail.com>
2020-10-23 19:50:38 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# WhatsApp\nWeb App
+
+A Whatsapp webview(desktop) Android app for easy use when you have two phones and want to share whatsapp without need to login or sharing the whatsapp media files.
+
+## Info About the App
+It is just a Simple app to seamlessly use whatsapp across devices. You would think that a Chrome App with the link(for Whatsapp web) is just enough but sometimes you would have many chrome tabs opened it might be take a lot of ram in your phone. So for this a Web App for WhatsApp Web is made to make your life easier. 
+
+## How to use
+Just install the App and follow the instructions.
+Download the App [here](https://github.com/Poujhit). Download the correct architecture version.
+## How this Project is Made
+
+This project is made using flutter framework. 
+
+These are the Packages used for this app. 
+
+- [Launcher icons for android and ios](https://pub.dev/packages/flutter_launcher_icons)
+- [Web View Plugin](https://pub.dev/packages/webview_flutter)
+
+To See my Other Projects
+- [My Github Profile](https://github.com/Poujhit) 
+About Flutter 
+- [Flutter](https://flutter.dev), 
+
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Map<int, Color> color = {
+    50: Color.fromRGBO(136, 14, 79, .1),
+    100: Color.fromRGBO(136, 14, 79, .2),
+    200: Color.fromRGBO(136, 14, 79, .3),
+    300: Color.fromRGBO(136, 14, 79, .4),
+    400: Color.fromRGBO(136, 14, 79, .5),
+    500: Color.fromRGBO(136, 14, 79, .6),
+    600: Color.fromRGBO(136, 14, 79, .7),
+    700: Color.fromRGBO(136, 14, 79, .8),
+    800: Color.fromRGBO(136, 14, 79, .9),
+    900: Color.fromRGBO(136, 14, 79, 1),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: MaterialColor(0xFF1b5e20, color),
+      ),
+      darkTheme: ThemeData.dark(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WhatsAppWeb App'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            onPressed: () => showAboutDialog(
+              context: context,
+              applicationIcon: SizedBox(
+                height: 40,
+                width: 40,
+                child: Image.asset('images/whatsapp.png'),
+              ),
+              applicationName: 'WhatsApp\nWeb App',
+              applicationLegalese: '''MIT License
+
+Copyright (c) 2020 MU Poujhit
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.''',
+              applicationVersion: '1.0.1',
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: WebView(
+          initialUrl: 'https://web.whatsapp.com/',
+          javascriptMode: JavascriptMode.unrestricted,
+          userAgent:
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
+        ),
+      ),
+    );
+  }
+}
