c7e1c2868c607c7b8e9a152c4b8c0c1c5e773104
Poujhit <poujhit.dev@gmail.com>
2021-01-12 16:11:02 +0530

    fix: code refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:whatsappweb/license.dart';
+
+import 'package:whatsappweb/whatsapp_webview.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,51 +31,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: MaterialColor(0xFF1b5e20, color),
       ),
       darkTheme: ThemeData.dark(),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('WhatsAppWeb App'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.more_vert,
-            ),
-            onPressed: () => showAboutDialog(
-              context: context,
-              applicationIcon: SizedBox(
-                height: 40,
-                width: 40,
-                child: Image.asset('images/whatsapp.png'),
-              ),
-              applicationName: 'WhatsApp\nWeb App',
-              applicationLegalese: License.licenseText,
-              applicationVersion: '1.0.2',
-            ),
-          )
-        ],
-      ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        child: WebView(
-          initialUrl: 'https://web.whatsapp.com/',
-          javascriptMode: JavascriptMode.unrestricted,
-          userAgent:
-              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
-        ),
-      ),
+      home: WhatsAppWebView(),
     );
   }
 }
